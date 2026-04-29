fca4471856de3965e0d26cf8234a6610295276ff
William Ramos <williamgomesramosp@gmail.com>
2026-04-29 17:15:18 -0300

    Adicionar arquivo index

---
+++ PayDay/app/index.tsx
@@ -0,0 +1,19 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>PAY DAY</Text>
+      <Text>INICIAR</Text>
+      <Text>VOLTAR</Text>
+      <Text>EFETUAR PAGAMENTO</Text>
+    </View>
+  );
+}
+
