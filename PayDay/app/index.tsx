f68c2a92af5e9b72f8cc8cb3bf40b5645c879d08
William Ramos <williamgomesramosp@gmail.com>
2026-05-06 16:27:33 -0300

    cd

--- PayDay/app/index.tsx
+++ PayDay/app/index.tsx
@@ -1,5 +1,4 @@
-import { Text, View } from "react-native";
-
+import { Text, View, Button } from "react-native";
 export default function Index() {
   return (
     <View
@@ -10,9 +9,52 @@ export default function Index() {
       }}
     >
       <Text>PAY DAY</Text>
-      <Text>INICIAR</Text>
-      <Text>VOLTAR</Text>
-      <Text>EFETUAR PAGAMENTO</Text>
+      <Button
+  onPress={() => null}
+  title="LOGIN"
+  color="#841584"
+  accessibilityLabel="Login com Google ou com E-Mail"
+    >
+    </Button>
+    <Button 
+  onPress={() => null}
+  title="INICIAR"
+  color="#841584"
+  accessibilityLabel="Entrar no app se já estiver logado"
+  >
+   </Button>
+   <Button
+  onPress={() => null}
+  title="PAGAR"
+  color="#841584"
+  accessibilityLabel="Efetuar pagamento com cartão ou pix"
+  >
+  </Button>,
+  <Button
+  onPress={() => null}
+  title="IMPRIMIR BOLETO"
+  color="#841584"
+  accessibilityLabel="Imprimir boleto de comprovante de pagamento"
+  >
+  </Button>
+  <Button
+  onPress={() => null}
+  title="ENVIAR COMPROVANTE"
+  color="#841584"
+  accessibilityLabel="Enviar comprovante de pagamento por whatsapp"
+  >
+  </Button>
+<Button
+  onPress={() => null}
+  title="SAIR"
+  color="#841584"
+  accessibilityLabel="Sair de aplicativo"
+/>
+/>
+/>
+/>
+
+
     </View>
   );
 }
